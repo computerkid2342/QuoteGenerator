3b78d65399f5aff9bef4b7f1710ec8bd9f4fac94
Rishik Durvasula <luckyice55@gmail.com>
2024-08-31 22:05:12 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'; //react components
+import ReactDOM from 'react-dom'; //hook onto the element the react application will be contained in
 
-import App from './App';
+import App from './App'; //main component
 
-ReactDOM.render(<App />, document.getElementById('root') );
+ReactDOM.render(<App />, document.getElementById('root') ); // render App and hook onto the "root"
